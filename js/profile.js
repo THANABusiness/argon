493dd6a0e71bf9e34176d537a27cfb9c946b2b59
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2023-05-27 03:31:14 +0700

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,5 @@
 const profile = document.getElementById('profile');
-profile.innerHTML = `<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-   
-    <link rel="stylesheet" href="https://onelink-nu.vercel.app/_nuxt/entry.fe9334aa.css">
-    <link rel="stylesheet" href="https://onelink-nu.vercel.app/_nuxt/Icon.6eb7e0ad.css">
-  </head>
-  <body>
-    <div id="__nuxt">
-      <!--[-->
-      <!--[-->
-      <!--[-->
-      <div>
+profile.innerHTML = `
         <main class="p-4 bg-white h-full w-full space-y-8 pt-12 max-w-lg mx-auto">
           <div class="text-center">
             <div class="h-20 w-20 rounded-full overflow-hidden ring ring-slate-300 mx-auto">
@@ -21,15 +7,15 @@ profile.innerHTML = `<html>
             </div>
                <p class="text-sm mt-2 text-slate-600">My Account</p>
             <h1 class="text-2xl font-bold mt-4 text-slate-800">My Balance </h1>
+            
+    <h1>{{ 'customer.account.title' | t }}</h1>
          
           </div>
-          <!---->
+          
         </main>
-      </div>
-      <!--]-->
-      <!--]-->
-      <!--]-->
-    </div>
+      
+ 
+    
   
-  </body>
-</html>`;
+  
+`;
